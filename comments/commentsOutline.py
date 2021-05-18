91acea04c7895728df23da5f912971a79e134273
ZavonCamp <73620154+ZavonCamp@users.noreply.github.com>
2021-05-18 14:55:30 -0500

    kl

---
+++ comments/commentsOutline.py
@@ -0,0 +1,27 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#like a sentence. 
+
+#2) Make a multi-line comment.
+
+'''
+
+hahaha
+ialdkfaslkjfljasl;fd
+alidsfljasljfe
+
+
+adflkjaslkfjw
+
+
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+ok = 3 #this is the number three 
