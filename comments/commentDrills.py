4059313ffa781644d053f22cd3b802baeafd5ff7
ZavonCamp <73620154+ZavonCamp@users.noreply.github.com>
2021-05-18 15:00:45 -0500

    lkl

---
+++ comments/commentDrills.py
@@ -0,0 +1,45 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#single line comment
+
+#2) Make a multi-line comment.
+
+'''
+a
+multi-line comment
+
+a
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+A = 22 #A is 22 
+
+#4) Make a single line comment.
+
+#anotherone 
+
+#5) Make a multi-line comment.
+
+'''
+g
+and another one
+g
+'''
