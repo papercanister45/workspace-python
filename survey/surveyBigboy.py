d6e90c4ba833a0562ce302cc15d6a4362a3f2779
ZavonCamp <73620154+ZavonCamp@users.noreply.github.com>
2021-06-01 13:01:42 -0500

    yesm

---
+++ survey/surveyBigboy.py
@@ -0,0 +1,39 @@
+'''
+Create a survey for User
+
+@author: Zavon Campbell
+'''
+#Use input() to ask the user "What is your name?" and store that in
+#a variable called name. Then print the variable.
+
+name = input("what is your name?")
+
+print(name) 
+
+#Use input() to ask the user "What is your favorite color?" and store that 
+#in a variable called color. Then print the variable.
+
+color = input("what is your favorite color?")
+
+print(color)
+
+#Use input() to ask the user "What city did you grow up in?" and store 
+#that in a variable called city. Then print the variable.
+
+city = input("what city did you grow up in?")
+
+print(city)
+
+#Use input() to ask the user "What is your best friends name?" and store 
+#that in a variable called friend. Then print the variable.
+
+friend = input("what is your best friends name?")
+
+print(friend)
+
+#Use input() to ask the user "What color hair do you have?" and store that 
+#in a variable called hair. Then print the variable.
+
+hair = input("what color hair do you have?")
+
+print(hair)
