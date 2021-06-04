dadd1e9013bdd1ee18a7511260741b4d3a369c26
ZavonCamp <73620154+ZavonCamp@users.noreply.github.com>
2021-06-04 12:23:12 -0500

    worowe

---
+++ math/mathOutline.py
@@ -0,0 +1,62 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+
+orangeJuice = "orange" + "juice" 
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+
+twentyOne = 9 + 10  
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+
+zero = 20 - 20 
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+
+pointOne = 1.00 - 0.99
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+
+rabbit = 2 * 3 
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+
+carrot = "rabbit" * 30 
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+
+forty = 80 / 20 
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+
+boat = 0.45 / 0.45 
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+
+yes = 37 % 44 
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+
+remainder = 55 // 2 
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+
+up = 33 ** 333
+
+#12) Make any type of operation with a common SYNTAX error
+
+"yup" = nope + 2342
