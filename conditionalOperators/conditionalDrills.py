80b45d28f95525bf021acff84e035c31c9849e11
ZavonCamp <73620154+ZavonCamp@users.noreply.github.com>
2021-07-12 12:38:15 -0500

    jvjv

--- conditionalOperators/conditionalDrills.py
+++ conditionalOperators/conditionalDrills.py
@@ -9,38 +9,43 @@ For this assignment you should read the task, then below the task do what it ask
 
 grade = 22
 
-if grade=>80:
+if grade>=80:
     print("passing")
 
-elif grade=<60:
+elif grade>=60 or grade<=80:
     print("probation")
+else:
+    print("failing")
 #2) Make a int variable named a. Now make an if/elif statment, and if a is more
 #than/equal too 100 or less than/equal too -100, print "a is a three digit 
 #number". Elif a is between 100 and -100, print "a is not a three digit number".
 
 a = 55
 
-    if a >= 100 or a <= -100
-        print("a is a three digit") 
-    elif a = 100 and -100
+if a >= 100 or a <= -100:
+    print("a is a three digit number") 
+elif a <= 100 and a >= -100:
+    print("a is not a three digit number")
 #3)Create a variable called age. Make an if/else statement. If age is older
 #than 18, print "You can vote". Else, print "You cannot vote".
 
-age if > 18:
+age = 18
+
+if age > 18:
     print("you can vote")
-    else 
+else: 
     print("you cannot vote")
     
-
 #4)Make a string variable called answer, and set it to the letter a, b, c, or d.
 #Make an if/elif statement. If answer is a, b, or d, print "Wrong". Elif answer
 #is c, print "Correct".
 
-"answer" = a, b, c, or d:
-    if "answer" = a, b, or de
-    print("wrong")
-    elif "answer" = c
+d = "answer"
+
+if d == "answer":
     print("correct")
+else: 
+    print("wrong")
 #5)Make two boolean variables. Make an if/elif/else statement. If both booleans 
 #are true, print "Both". Elif only one of the booleans is true, print "Only 
 #one". Else, print "Neither".
@@ -49,9 +54,9 @@ a =True
 
 b= False 
 
-if a or b 
+if a and b: 
     print("both")
-elif a 
+elif a or b: 
     print("only one")
-else b
+else:
     ("neither")
